6350bcaa6f80dd1c2c88ffdf30c010a89aa3f0fa
Ramiro Fassetta <ramiro.fassetta.01@gmail.com>
2023-12-19 17:21:02 -0300

    estilos y forma a la pag principal

--- sistema de ventas/src/components/ProductosHome.jsx
+++ sistema de ventas/src/components/ProductosHome.jsx
@@ -6,14 +6,12 @@ export const ProductosHome = () => {
 
     <div className="flex flex-row">
       <div className="space-y-10 ml-20 w-2/4 ">
-        <div className="border border-gray-300 rounded-md p-5">Bebidas</div>
-        <div className="border border-gray-300 rounded-md p-5">
-          Productos Empaquetados
-        </div>
-        <div className="border border-gray-300 rounded-md p-5">Lacteos</div>
-        <div className="border border-gray-300 rounded-md p-5">Panadería</div>
-        <div className="border border-gray-300 rounded-md p-5">Embutidos</div>
-        <div className="border border-gray-300 rounded-md p-5">
+        <div className="border border-gray-300 rounded-md p-5 font-raleway">Bebidas</div>
+        <div className="border border-gray-300 rounded-md p-5 font-raleway">Productos Empaquetados</div>
+        <div className="border border-gray-300 rounded-md p-5 font-raleway">Lacteos</div>
+        <div className="border border-gray-300 rounded-md p-5 font-raleway">Panadería</div>
+        <div className="border border-gray-300 rounded-md p-5 font-raleway">Embutidos</div>
+        <div className="border border-gray-300 rounded-md p-5 font-raleway">
           Cuidado Personal y otros
         </div>
       </div>
--- sistema de ventas/src/components/SumaProductos.jsx
+++ sistema de ventas/src/components/SumaProductos.jsx
@@ -1,20 +1,21 @@
 
 
 export const SumaProductos = () => {
+  
   return (
     <div className="border border-gray-300 ml-36 rounded-md w-2/4 mr-20">
-      <h1 className="ml-16 mt-10 text-lg">Total Suma</h1>
+      <h1 className="ml-16 mt-10 text-lg font-raleway font-semibold">Total Suma</h1>
       <div className="w-full border border-gray-200 mt-5"></div>
 
       <div >
         {/* hago 3 productos estaticos y desp le agrego funcionalidad para agregar */}
-        <div className="ml-16 mb-5 mt-5 border-2 border-gray-400 rounded-md p-5 mr-16">
+        <div className="ml-16 mb-5 mt-5 border-2 border-gray-300 rounded-md p-5 mr-16 font-raleway ">
           *foto* Producto 1 <span className="">x1</span><span className="">$300</span>
         </div>
-        <div className="ml-16 mb-5 mt-5 border-2 border-gray-400 rounded-md p-5 mr-16">
+        <div className="ml-16 mb-5 mt-5 border-2 border-gray-300 rounded-md p-5 mr-16 font-raleway ">
           *foto* Producto 2 <span>x1</span><span className="">$300</span>
         </div>
-        <div className="ml-16 mb-5 mt-5 border-2 border-gray-400 rounded-md p-5 mr-16">
+        <div className="ml-16 mb-5 mt-5 border-2 border-gray-300 rounded-md p-5 mr-16 font-raleway ">
           *foto* Producto 2 <span>x1</span><span className="">$300</span>
         </div>
       </div>
@@ -22,7 +23,7 @@ export const SumaProductos = () => {
       <div className="w-full border border-gray-200 mt-5"></div>
 
       <div className="">
-        <p className="ml-16 mt-5">Total: <span>$900</span></p>
+        <p className="ml-16 mt-5 font-raleway font-semibold text-green-500 text-xl">Total: <span>$900</span></p>
       </div>
     </div>
   )
