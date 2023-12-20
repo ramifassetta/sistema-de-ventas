bbedb5df534bd30e6fe7ff26f8d0c133b0f34274
Ramiro Fassetta <ramiro.fassetta.01@gmail.com>
2023-12-20 16:52:56 -0300

    pequeño cambio en la nav

--- sistema de ventas/src/components/Navbar.jsx
+++ sistema de ventas/src/components/Navbar.jsx
@@ -2,13 +2,13 @@
 export const Navbar = () => {
 
   return (
-    <nav className="bg-indigo-600 p-4">
+    <nav className="bg-indigo-600 p-2">
       <div className="container mx-auto flex justify-between items-center">
         <a href="/home" className="text-2xl text-white font-extrabold text-center">
           Sistema de Ventas
         </a>
         <ul className="my-5 text-sm md:flex md:items-center md:gap-3 font-bold text-white ">
-          <li>
+          <li className="bg-indigo-800 rounded-md p-2 pl-3 pr-3">
             <a href="">Productos</a>
           </li>
         </ul>
